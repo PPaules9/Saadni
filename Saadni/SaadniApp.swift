b119a131fe08351bb4c1a106be27e15069d5c2dd
ppaules9 <pavlypaules9@gmail.com>
2026-02-06 13:14:20 +0200

    Initial Commit

---
+++ Saadni/SaadniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaadniApp.swift
+//  Saadni
+//
+//  Created by Pavly Paules on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SaadniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
